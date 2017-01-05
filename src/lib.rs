eb14261a36e0a44fe28e2cf1e6ec41b2cac5337a
Pepijn de Vos <pepijndevos@gmail.com>
2017-01-05 16:52:43 +0100

    uart printing

--- src/lib.rs
+++ src/lib.rs
@@ -6,38 +6,14 @@
 extern crate rlibc;
 extern crate compiler_builtins;
 
-use core::ptr::{read_volatile, write_volatile};
+use core::fmt::Write;
 
-//const UART_THR: *mut u8 = 0x101f1000 as *mut u8;
-const UART_THR: *mut u8 = 0x01D0C000 as *mut u8;
-const UART_LSR: *mut u8 = 0x01D0C014 as *mut u8;
+mod uart;
+mod gpio;
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-    putchar('v');
-    putchar('o');
-    putchar('s');
-    unsafe {
-        /*
-        *UART_THR = 'v' as u8;
-        asm!("strb $1, [$0]"
-        : // ouputs
-        : "r"(UART_THR), "r"('d' as u8) // inputs
-        : // clobbers
-        : // options
-        );
-        while (read_volatile(UART_LSR) & (1 << 5)) == 0 {}
-        write_volatile(UART_THR, 'v' as u8);
-        */
-    }
-}
-
-fn putchar(ch: char) {
-    //let a = ch as u8 + 1;
-    unsafe {
-        while (read_volatile(UART_LSR) & (1 << 5)) == 0 {}
-        write_volatile(UART_THR, ch as u8);
-    }
+    write!(uart::UART0, "hello {}\r\n", "Pepijn").unwrap();
 }
 
 #[lang = "panic_fmt"]
@@ -47,3 +23,5 @@ pub extern fn panic_fmt() -> ! {loop{}}
 // something is inserting annoying functions in my code
 #[no_mangle]
 pub extern "C" fn __aeabi_unwind_cpp_pr0() {}
+#[no_mangle]
+pub extern "C" fn __aeabi_unwind_cpp_pr1() {}
---
+++ src/uart.rs
@@ -0,0 +1,21 @@
+use core::ptr::{read_volatile, write_volatile};
+use core::fmt::{Write, Result};
+
+pub struct Uart {
+    thr: *mut u8,
+    lsr: *mut u8,
+}
+
+pub const UART0 : Uart = Uart { thr: 0x01D0C000 as *mut u8, lsr: 0x01D0C014 as *mut u8 };
+
+impl Write for Uart {
+    fn write_str(&mut self, s: &str) -> Result {
+        for ch in s.bytes() {
+            unsafe {
+                while (read_volatile(self.lsr) & (1 << 5)) == 0 {}
+                write_volatile(self.thr, ch);
+            }
+        }
+        return Ok(());
+    }
+}
